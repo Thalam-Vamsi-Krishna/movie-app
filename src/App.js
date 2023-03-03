096bae33462d431a7e071c2597fda7f12bdddb98
Thalam-Vamsi-Krishna <vamsikrishna.thalam@gmail.com>
2023-03-03 20:17:47 +0530

    successfully implemented

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
     try {
+      setIsLoading(true);
       const response = await fetch("https://swapi.dev/api/films/");
       const data = await response.json();
       const transformedMovies = data.results.map((movieData) => {
@@ -19,17 +21,21 @@ function App() {
         };
       });
       setMovies(transformedMovies);
+      setIsLoading(false);
     } catch (error) {
       console.log("An error occurred:", error);
     }
   }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
